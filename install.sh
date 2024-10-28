a858545df385ca68ff28b6b2e56c3b7d628a6aec
Azazel <azazelwoodwind11@gmail.com>
2024-10-28 02:07:04 +0000

    add sudo and correct filename

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 sudo pacman -Syu
 sudo pacman -S --needed reflector
 sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist
-sudo pacman -S --needed - < packages.txt
+sudo pacman -S --needed - < pacman-packages.txt
 sudo paru -S --needed - < paru-packages.txt
 
-curl -fsSL https://bun.sh/install | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+sudo curl -fsSL https://bun.sh/install | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-cp -r fish ~/.config/
-cp .tmux.conf ~/
-cp starship.toml ~/.config/
-cp .bashrc ~/
-cp .bash_profile /root
+sudo cp -r fish ~/.config/
+sudo cp .tmux.conf ~/
+sudo cp starship.toml ~/.config/
+sudo cp .bashrc ~/
+sudo cp .bash_profile /root
 
-chsh -s /usr/bin/fish
+sudo chsh -s /usr/bin/fish
 fish -c 'fish_add_path ~/.pyenv/bin'
