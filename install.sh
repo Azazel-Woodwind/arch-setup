c1e32236c24cd15a3eeacc106c1560f73859b81f
Azazel <azazelwoodwind11@gmail.com>
2024-10-28 03:13:25 +0000

    set locale and remove domain specific env variables

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+echo "en_GB.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+sudo locale-gen
+
 sudo pacman -Syu
 sudo pacman -S --needed reflector
 sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist
 sudo pacman -S --needed - < pacman-packages.txt
 rustup default stable
-sudo git clone https://aur.archlinux.org/paru.git
+git clone https://aur.archlinux.org/paru.git
 cd paru
-sudo chown -R $USER $(pwd)
 makepkg -si
 cd ..
 
@@ -15,10 +17,11 @@ sudo paru -S --needed - < paru-packages.txt
 curl -fsSL https://bun.sh/install | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
+mkdir -p ~/.config/fish
 cp -r fish ~/.config/
 cp .tmux.conf ~/
 cp starship.toml ~/.config/
 cp .bashrc ~/
 sudo cp .bash_profile /root
 chsh -s /usr/bin/fish
-fish -c 'fish_add_path ~/.pyenv/bin'
+sudo fish -c 'fish_add_path ~/.pyenv/bin'
